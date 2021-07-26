7668d27d9bab24747b39b00f86ba511c6a8b0bce
ThinhVu <vutrongthinhk7@gmail.com>
2021-07-26 11:35:00 +0700

    Initial commit

---
+++ src/main/java/com/sfbl/java/delegate/IFunc.java
@@ -0,0 +1,21 @@
+package com.sfbl.java.delegate;
+
+public class IFunc {
+    public interface _0 { void execute(); }
+    public interface _1<T> { T execute(); }
+    public interface _2<T1, T2> { T2 execute(T1 p1); }
+    public interface _3<T1, T2, T3> { T3 execute(T1 p1, T2 p2); }
+    public interface _4<T1, T2, T3, T4> { T4 execute(T1 p1, T2 p2, T3 p3); }
+    public interface _5<T1, T2, T3, T4, T5> { T5 execute(T1 p1, T2 p2, T3 p3, T4 p4); }
+    public interface _6<T1, T2, T3, T4, T5, T6> { T6 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5); }
+    public interface _7<T1, T2, T3, T4, T5, T6, T7> { T7 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6); }
+    public interface _8<T1, T2, T3, T4, T5, T6, T7, T8> { T8 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7); }
+    public interface _9<T1, T2, T3, T4, T5, T6, T7, T8, T9> { T9 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8); }
+    public interface _10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> { T10 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9); }
+    public interface _11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> { T11 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10); }
+    public interface _12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> { T12 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10, T11 p11); }
+    public interface _13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> { T13 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10, T11 p11, T12 p12); }
+    public interface _14<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> { T14 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10, T11 p11, T12 p12, T13 p13); }
+    public interface _15<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> { T15 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10, T11 p11, T12 p12, T13 p13, T14 p14); }
+    public interface _16<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> { T16 execute(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9, T10 p10, T11 p11, T12 p12, T13 p13, T14 p14, T15 p15); }
+}
